c3f6cd0530351b94091287a563f26c0a32bf4557
Don Naro <dnaro@redhat.com>
2022-10-26 09:57:10 +0100

    set up ansible-lint title

--- bin/build_rules_assembly.sh
+++ bin/build_rules_assembly.sh
@@ -2,11 +2,15 @@
 
 ######
 # This script creates an assembly that contains Ansible-lint rule documentation.
+# Resulting assembly requires manual intervention to put adoc files in alphabetical order.
 ######
 
+# Create an assembly with all the adoc rules.
 for f in $(find ../sync/lint-docs/rules -name '*.adoc')
 do
   echo "$f"
-done > ../sync/lint-docs/assembly_lint_rules.adoc
-sed -i -e 's|../sync/lint-docs/rules|include::rules|g' \
--e 's|.adoc|.adoc\[leveloffset=+1\]|g' ../sync/lint-docs/assembly_lint_rules.adoc
+done > ../titles/ansible-lint/assembly_lint_rules.adoc
+
+# Replace the path with an include directive and add a leveloffset.
+sed -i -e 's|../sync/|include::|g' \
+ -e 's|.adoc|.adoc\[leveloffset=+1\]|g' ../titles/ansible-lint/assembly_lint_rules.adoc
