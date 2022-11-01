18bb091a6b98646bd6e877f45a5fb5fec76feb27
Don Naro <dnaro@redhat.com>
2022-11-01 11:30:44 +0000

    additional ag and ug xrefs

--- bin/replace-controller-xrefs.sh
+++ bin/replace-controller-xrefs.sh
@@ -3,6 +3,7 @@
 adocfiles=$(find ../sync/controller-docs/ -name '*.adoc')
 
 # Replace unknown ID or title for internal cross references
+# Administration Guide cross-refs
 sed -i -e 's/xref:ag_backup_restore\[\]/{ag_backup_restore}/g' \
  -e 's/xref:ag_clustering\[\]/{ag_clustering}/g' \
  -e 's/xref:ag_configure_tower\[\]/{ag_configure_tower}/g' \
@@ -19,7 +20,11 @@ sed -i -e 's/xref:ag_backup_restore\[\]/{ag_backup_restore}/g' \
  -e 's/xref:ag_social_auth`/{ag_social_auth}/g' \
  -e 's/xref:ag_topology_viewer\[\]/{ag_topology_viewer}/g' \
  -e 's/xref:ag_use_oauth_pat\[\]/{ag_use_oauth_pat}/g' \
- -e 's/xref:ug_build_ees\[\]/{ug_build_ees}/g' \
+ -e 's/xref:ag_oauth2_token_revoke\[\]/{ag_oauth2_token_revoke}/g' $adocfiles
+
+# Replace unknown ID or title for internal cross references
+# User Guide cross-refs
+sed -i -e 's/xref:ug_build_ees\[\]/{ug_build_ees}/g' \
  -e 's/xref:ug_credentials_add\[\]/{ug_credentials_add}/g' \
  -e 's/xref:ug_execution_environments\[\]/{ug_execution_environments}/g' \
  -e 's/xref:ug_inventories_add\[\]/{ug_inventories_add}/g' \
@@ -35,7 +40,16 @@ sed -i -e 's/xref:ag_backup_restore\[\]/{ag_backup_restore}/g' \
  -e 's/xref:ug_webhooks\[\]/{ug_webhooks}/g' \
  -e 's/xref:ug_manual\[\]/{ug_manual}/g' \
  -e 's/xref:ug_projects_scm_types\[\]/{ug_projects_scm_types}/g' \
+ -e 's/xref:ug_customscripts\[\]/{ug_customscripts}/g' \
+ -e 's/xref:ug_inventory_sources\[\]/{ug_inventory_sources}/g' \
+ -e 's/xref:ug_inventories\[\]/{ug_inventories}/g' \
+ -e 's/xref:ug_credentials_ocp_k8s\[\]/{ug_credentials_ocp_k8s}/g' \
+ -e 's/xref:ug_activitystreams\[\]/{ug_activitystreams}/g' \
+ -e 's/xref:ug_applications_auth\[\]/{ug_applications_auth}/g' \
+ -e 's/xref:ug_galaxy\[\]/{ug_galaxy}/g' \
+ -e 's/xref:ug_ldap_auth_perf_tips\[\]/{ug_ldap_auth_perf_tips}/g' \
  -e 's/xref:ug_scheduling\[\]/{ug_scheduling}/g' $adocfiles
 
 # Replace other cross references
+# 
 sed -i -e 's/Token and session management <administration:ag_token_utility>/{ag_token_utility}/g' $adocfiles
