6506029cb556a74648ee1ea08d8a34cec7fed010
Aine Riordan <ariordan@redhat.com>
2023-10-05 12:53:12 +0100

    Add lightspeed sync script

---
+++ bin/sync_lightspeed_docs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+######
+# This script synchronizes content to the downstream repository.
+# There is a Jenkins job that configures the source and target repositories and runs this script directly.
+# If you commit changes to this script you should verify the Jenkins job runs successfully.
+######
+
+# Set the path to the source and target directories.
+# The source directory contains the content that you want to synchronize.
+source=source
+# The target directory is the location where you want to synchronize content.
+target=target
+
+# Clean the existing downstream and release-note folders.
+rm -rf $target/lightspeed
+
+# Copy the content of the downstream and release-note folders.
+cp -r $source/lightspeed $target/lightspeed
