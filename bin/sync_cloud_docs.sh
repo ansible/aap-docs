ff5b844a935c384ae04485a649c375c25b0f3f64
Aine Riordan <44700011+ariordan-redhat@users.noreply.github.com>
2025-10-27 10:59:38 +0000

    Restructure clouds for mirroring (#4816)

--- bin/sync_cloud_docs.sh
+++ bin/sync_cloud_docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ######
-# This script synchronizes cloud content to the AAP docs downstream repository in Gitlab.
+# This script synchronizes cloud content to the AAP docs clouds/ repository in Gitlab.
 # A Jenkins job configures the source and target repositories and runs this script directly.
 # If you commit changes to this script, verify the Jenkins job runs successfully.
 ######
@@ -12,34 +12,8 @@ source=source
 # The target directory is the location where you want to synchronize content.
 target=target
 
-# Clean the target directories.
-# rm -rf $target/titles/aap-on-aws
-rm -rf $target/aap-on-azure/aap-common
-rm -rf $target/aap-on-azure/attributes
-rm -rf $target/aap-on-azure/images
-rm -rf $target/aap-on-azure/stories
-rm -rf $target/aap-on-azure/stories.adoc
+# Clean the existing downstream and release-note folders.
+rm -rf $target/aap-clouds
 
-rm -rf $target/saas-aws/aap-common
-rm -rf $target/saas-aws/attributes
-rm -rf $target/saas-aws/images
-rm -rf $target/saas-aws/stories
-rm -rf $target/saas-aws/stories.adoc
-
-# Copy aap-common to the target directories.
-cp -r $source/aap-common/ $target/aap-on-azure/
-cp -r $source/aap-common/ $target/saas-aws/
-
-# Copy attributes to the target directories.
-cp -r $source/attributes/ $target/aap-on-azure/
-cp -r $source/attributes/ $target/saas-aws/
-
-# Copy images to the target directories.
-cp -r $source/images/ $target/aap-on-azure/
-cp -r $source/images/ $target/saas-aws/
-
-# Copy user stories to the target directories.
-cp -r $source/stories/ $target/saas-aws/
-cp -r $source/titles/saas-aws/stories.adoc $target/saas-aws/
-cp -r $source/stories/ $target/aap-on-azure/
-cp -r $source/titles/aap-on-azure/stories.adoc $target/aap-on-azure/
+# Copy the content of the downstream and release-note folders.
+cp -r $source/aap-clouds $target/aap-clouds
